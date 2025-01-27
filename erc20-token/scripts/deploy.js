d98b07eab8b02aaf7ab8bb06ab09400d592290ad
M-Jey <mjeyict3@gmail.com>
2025-01-27 12:35:00 +0800

    Add deployment script for ERC20 token contract

---
+++ erc20-token/scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contract with the account:", deployer.address);
+
+    const MyToken = await ethers.getContractFactory("MyToken");
+    const token = await MyToken.deploy("MyToken", "MTK", 1000000); // Name, Symbol, Initial Supply
+
+    console.log("Token deployed to:", token.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
